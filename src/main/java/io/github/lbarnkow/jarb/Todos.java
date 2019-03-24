a6955c3dbaae1ecd0c1fe63bfc048fd17c6d1936
lbarnkow <13865903+lbarnkow@users.noreply.github.com>
2019-03-24 20:25:28 +0100

    added TODO list to collect future tasks and ideas

---
+++ src/main/java/io/github/lbarnkow/jarb/Todos.java
@@ -0,0 +1,22 @@
+// Kept as .java file to use eclipse IDE's built-in task tracking
+// This file will go through the Java compiler; so keep it compilable!
+
+// TODO: build simple pugme-bot as baseline proof-of-concept
+// TODO: learn travis
+// TODO: CI: setup travis
+// TODO: CI: execute tests
+// TODO: CI: get code coverage
+// TODO: CI: upload coverage report to codecov (branch master)
+// TODO: CI: owasp dependency scan
+// TODO: CI: dependency updates
+// TODO: setup libraries.io
+// TODO: add badges to README: build-status (travis)
+// TODO: add badges to README: codecoverage
+// TODO: add badges to README: libraries.io (ideally for outdated, as well as, insecure deps)
+// TODO: look into semantic commits?
+// TODO: look into automatically manage versioning?
+// TODO: create a docker hub account to publish pre-built images?
+// TODO: CI: build an publish docker image (master & develop)
+// TODO: make persistent key-value-store available to bots (JetBrains Xodus?)
+// ----- https://github.com/jetbrains/xodus
+// ----- https://github.com/JetBrains/xodus/wiki/Entity-Stores
